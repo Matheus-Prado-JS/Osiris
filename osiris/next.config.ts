fc817fa465bfc5fcd209a8034b5d30944570ad10
Matheus Prado <pradomatheus937@gmail.com>
2026-05-28 15:53:44 -0300

    fix static export

--- osiris/next.config.ts
+++ osiris/next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   output: "export",
-
-  basePath: "/osiris",
-
-  assetPrefix: "/osiris/",
-
+  trailingSlash: true,
 };
 
 export default nextConfig;
